e1989afbc871c5e874cb88d030ccd34c968bd3e7
jeet kanodia <jeetkanodia@gmail.com>
2024-11-14 23:38:17 +0530

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "350px",
+      },
+      colors: {
+        primaryColor: "#573CFA",
+        yellowColor: "#FB8D1A",
+        blackColor: "#1C1A27",
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        tertiary: "var(--tertiary-color)",
+        textColor: "var(--text-color)",
+        filledBg: "var(--filled-color)",
+        bgColor: "var(--bg-color)",
+        mainBg: "var(--main-bg)",
+      },
+
+      fontFamily: {
+        primary: "var(--primary-font)",
+        secondary: "var(--secondary-font)",
+      },
+    },
   },
-  plugins: [],
 };
